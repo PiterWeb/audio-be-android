72783845348ae7e7f8afa1e813e5b7ac4924aef2
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-06-26 19:34:04 +0200

    Working on notification

--- app/src/main/java/com/piterdev/audiobe/PlaybackService.kt
+++ app/src/main/java/com/piterdev/audiobe/PlaybackService.kt
@@ -1,18 +1,29 @@
 package com.piterdev.audiobe
 
 
+import android.os.Bundle
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.CommandButton
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.SessionCommand
+import com.google.common.collect.ImmutableList
 
 class PlaybackService : MediaSessionService() {
     private var mediaSession: MediaSession? = null
+    private val customCommandFavorites = SessionCommand("ACTION_PREVIOUS", Bundle.EMPTY)
 
     // Create your Player and MediaSession in the onCreate lifecycle event
+    @OptIn(UnstableApi::class)
     override fun onCreate() {
         super.onCreate()
+
+        val previousButton = CommandButton.Builder(CommandButton.ICON_PREVIOUS).setDisplayName("Previous").setSessionCommand(customCommandFavorites).build()
+
         val player = ExoPlayer.Builder(this).build()
-        mediaSession = MediaSession.Builder(this, player).setMediaButtonPreferences().build()
+        mediaSession = MediaSession.Builder(this, player).setMediaButtonPreferences(ImmutableList.of(previousButton)).build()
     }
 
     // Remember to release the player and media session in onDestroy
